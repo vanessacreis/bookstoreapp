91ff764b2c44391bdaa45a1f5adc3d6cac47d120
vanessacreis <vanessacreis@live.com>
2022-04-17 14:36:59 -0300

    Implementação do footer na versão menor sem o logo

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import * as S from "./footer.js";
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <S.Copyright>
+        <p>&copy; Todos os direitos reservados</p>
+      </S.Copyright>
+      <S.Redes>
+        <img src="/assets/icons/instagram.svg" alt="Logo do Instagram" />
+        <img src="/assets/icons/twitter.svg" alt="Logo do Twitter" />
+        <img src="/assets/icons/facebook.svg" alt="Logo do Facebook" />
+      </S.Redes>
+    </S.Footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { colors } from "../../Style/globalStyle.js";
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 50px;
+  background-color: ${colors.blue};
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  position: absolute;
+  bottom: 0;
+`;
+
+export const Copyright = styled.div`
+  width: 45%;
+  color: ${colors.white};
+  font-family: ${colors.titles};
+  font-size: 13px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const Redes = styled.div`
+  width: 45%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  img {
+    width: 18px;
+    height: 18px;
+    margin-right: 15px;
+  }
+`;
