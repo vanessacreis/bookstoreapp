4f583072f3641b4a0364065759bb53cf3d9c6519
vanessacreis <vanessacreis@live.com>
2022-04-17 14:37:32 -0300

    Implementação do componente LargeFooter sem o logo

---
+++ src/Components/LargeFooter/LargeFooter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import * as S from "./largeFooter.js";
+
+const LargeFooter = () => {
+  return (
+    <S.Footer>
+      <S.Redes>
+        <p>
+          <img src="/assets/icons/instagram.svg" alt="" className="iconRedes" />
+          @InstadaLivraria
+        </p>
+        <p>
+          <img src="/assets/icons/twitter.svg" alt="" className="iconRedes" />
+          @TTDaLivraria
+        </p>
+        <p>
+          <img src="/assets/icons/facebook.svg" alt="" className="iconRedes" />
+          @FBDaLivraria
+        </p>
+      </S.Redes>
+      <hr className="separador" />
+      <S.Copyright>
+        <p>&copy; Todos os direitos reservados</p>
+      </S.Copyright>
+    </S.Footer>
+  );
+};
+
+export default LargeFooter;
---
+++ src/Components/LargeFooter/largeFooter.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { colors } from "../../Style/globalStyle.js";
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 120px;
+  background-color: ${colors.blue};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  position: absolute;
+  bottom: 0;
+
+  .separador {
+    width: 70%;
+  }
+`;
+
+export const Redes = styled.div`
+  width: 50%;
+  height: 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${colors.white};
+  font-family: ${colors.text};
+  font-size: 12px;
+  text-align: center;
+  text-transform: uppercase;
+
+  p {
+    display: flex;
+    align-items: center;
+  }
+
+  .iconRedes {
+    width: 15px;
+    height: 15px;
+    margin-right: 5px;
+  }
+`;
+
+export const Copyright = styled.div`
+  width: 50%;
+  height: 40%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  p {
+    color: ${colors.white};
+    font-family: ${colors.titles};
+    font-size: 13px;
+  }
+`;
