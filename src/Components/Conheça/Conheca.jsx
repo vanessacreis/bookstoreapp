5e2e378a9f153c64af505f6e769097033ec7a6ec
vanessacreis <vanessacreis@live.com>
2022-04-21 23:55:38 -0300

    Componente conheça

---
+++ src/Components/Conheça/Conheca.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as S from "./conheca.js";
+
+const Conheca = () => {
+  return (
+    <S.Conheca>
+      <h3>Conheça-nos</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias vel
+        cupiditate corrupti, praesentium tenetur deserunt.
+      </p>
+      <hr />
+      <img src="./assets/imgs/LivrariaIt.svg" alt="" />
+    </S.Conheca>
+  );
+};
+
+export default Conheca;
