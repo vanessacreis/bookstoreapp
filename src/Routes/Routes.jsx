7db115dbfd0fb457e333bccc4d0bcba7999c1876
vanessacreis <vanessacreis@live.com>
2022-04-21 23:57:02 -0300

    Corrigindo caminho da página home

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Header from "../Components/Header/Header.jsx";
-import Home from "../Pages/Home/Home";
+import Home from "../Pages/Home/Home.jsx";
 import { GlobalStyle } from "../Style/globalStyle.js";
 
 const Routes = () => {
