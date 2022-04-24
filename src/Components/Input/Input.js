76c1de37c1b95e63838d86ecf12f212d6b843728
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-24 19:24:30 -0300

    Estilizado o componente input menor

---
+++ src/Components/Input/Input.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { colors } from "../../Style/globalStyle";
+
+export const Input = styled.input`
+  width: 50%;
+  border-top: none;
+  border-right: none;
+  border-left: none;
+
+  color: ${colors.black};
+  font-size: ${colors.text};
+`;
