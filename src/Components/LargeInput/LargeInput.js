e576319c86783ad9ed57e657034e9521400ddff5
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-24 19:48:08 -0300

    estilizado o componente do input maior

---
+++ src/Components/LargeInput/LargeInput.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { colors } from "../../Style/globalStyle";
+
+export const LargeInput = styled.input`
+  width: 100%;
+  border-top: none;
+  border-right: none;
+  border-left: none;
+  color: ${colors.black};
+  font-size: ${colors.text};
+`;
