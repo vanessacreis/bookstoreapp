eb84c9f8e23a728e79c87e979ab6123bd3b38f86
Luzi <lu.gering@gmail.com>
2022-04-24 04:07:10 -0300

    CSS grid implementado.

--- src/Components/Container02_InfoLivro/index.js
+++ src/Components/Container02_InfoLivro/index.js
@@ -1,13 +1,16 @@
 import styled from "styled-components";
-export const Pai = styled.div`display: grid; align-content: start; row-gap: 50%
-padding: 15px;
->AreaDoLivreiro{color:blue}`
-export const InformacoesLivro = styled.div`width: 98%; margin-top: 5px
-> h1{font-size: 50px}
+export const Pai = styled.div`display: grid;
+padding: 30px 20px 30px 50px;
+>.org2 {display: grid; align-content: center; justify-content: center }
+>.org1 {background-color: rgba(20, 33, 38, 0.011)}`
+export const InformacoesLivro = styled.div`width: 98%; 
+
+> h1{font-size: 50px; color: green}
 > h3{}
 > p{}
 > button{margin-left: 50%; margin-top: 2%}`;
-export const AreaDoLivreiro = styled.div`text-align: center;  box-sizing: content-box; padding-bottom: 1%
+export const AreaDoLivreiro = styled.div`text-align: center; padding: 20px;
+ background-color: #FCFCFC;
 >h2{}
 >button{}
 `
