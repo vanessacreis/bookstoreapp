ab83b65b6f1d49fa3aed92bcd76496842e9d94fb
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-24 19:26:46 -0300

    importado componente input

---
+++ src/Pages/EditarLivro/EditarLivro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import Header from "../../Components/Header.jsx";
+import Footer from "../../Components/Footer/Footer.jsx";
+import Input from "../../Components/Input/Input.jsx";
+
+export default function EditarLivro() {
+  return (
+    <section>
+      <form action="">
+        <Input />
+      </form>
+      <Footer />
+    </section>
+  );
+}
