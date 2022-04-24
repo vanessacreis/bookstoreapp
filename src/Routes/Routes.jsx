04c269a3607c9f883bcf2041a6144466804234f8
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-24 19:27:05 -0300

    criado uma rota para página de edição do livro

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Header from "../Components/Header/Header.jsx";
 import Home from "../Pages/Home/Home";
+import EditarLivro from "../Pages/EditarLivro/EditarLivro.jsx";
 import { GlobalStyle } from "../Style/globalStyle.js";
 
 const Routes = () => {
@@ -11,6 +12,7 @@ const Routes = () => {
       <Header />
       <Switch>
         <Route path="/" element={<Home />} />
+        <Route path="/editar" element={<EditarLivro />} />
       </Switch>
     </BrowserRouter>
   );
