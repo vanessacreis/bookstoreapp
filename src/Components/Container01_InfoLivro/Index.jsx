4d50a32f02903ba8aaf28119e86d96c8723b90c1
Luzi <lu.gering@gmail.com>
2022-04-24 16:15:35 -0300

    Alterações pensadas na responsividade;

--- src/Components/Container01_InfoLivro/Index.jsx
+++ src/Components/Container01_InfoLivro/Index.jsx
@@ -3,15 +3,15 @@ import * as S from "./index.js"
 
 function Div1(){
     return(<S.ColunaPai>
-        <div>
-    <img src="https://miro.medium.com/max/1400/0*xRkytAftnlGPo3jK.jpg"></img>
-    <ul>
+         <S.Elementos>
+        <ul type="">
+        <img src="https://miro.medium.com/max/1400/0*xRkytAftnlGPo3jK.jpg"></img>
         <li>Editora:</li>
         <li>Genero:</li>
         <li>Páginas:</li>
         <li>País:</li>
         <li>Ano:</li>
-    </ul>
-    </div>
+        </ul>
+    </S.Elementos>
     </S.ColunaPai>);}
     export default Div1;
--- src/Components/Container01_InfoLivro/index.js
+++ src/Components/Container01_InfoLivro/index.js
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 export const ColunaPai = styled.div `
 background-color: #D9BFA0;
-width: 500px;
-margin-top: 0px; /* altura que está do topo */
-top: 0px; /* altura que vai parar antes do topo */
+display: grid;
+padding: 0px;
+height: 87vh;
+position: -webkit-sticky;
 position: sticky;
->div{
-    display: grid;
-    padding-bottom: 100%;
+top: 0;
+>ul{display: grid; padding: 0px}
+`
+export const Elementos = styled.div`
+    margin-left: 3%;
+    >ul{
     >img{
-        width:300px;
-        position:relative}
-
-}
-
-}
+        width:200px;
+        position: relative;
+        height: 300px;
+        }
+    }
 `;
