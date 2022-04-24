7d6f96863661e21abdcc0f5c26568b67fa661a79
Luzi <lu.gering@gmail.com>
2022-04-24 16:15:06 -0300

    Importação das fontes/ cores/ posicionamento

--- src/Components/Container02_InfoLivro/Index.jsx
+++ src/Components/Container02_InfoLivro/Index.jsx
@@ -5,13 +5,14 @@ function Div2 (){
         <S.Pai>
             <div className="org1">
 <S.InformacoesLivro>
-    <h1>hello World!</h1>
-    <h3>anhjsnd</h3>
+    <h1>Titulo do Livro</h1>
+    <h3>Autor</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. Enim adim ad minimim veniam, quis nostrud exercise ullamco laboris nisi ut alipip ex ea commodo consequat. Duis auteur irure pain in reprehenderit in voluptate velit this cillum dolore eu fugiat nulla pariatur. Excepteur sint occecat
          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <button>Reserve!</button>
     </S.InformacoesLivro>
     </div>
+    
     <div className="org2">
     <S.AreaDoLivreiro>
         <h2>Funcionalidades para livreiros</h2>
--- src/Components/Container02_InfoLivro/index.js
+++ src/Components/Container02_InfoLivro/index.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
+import { colors } from "../../Style/globalStyle.js";
 export const Pai = styled.div`display: grid;
-padding: 30px 20px 30px 50px;
+padding: 0px 0px 0px; bottom: 0px; 
 >.org2 {display: grid; align-content: center; justify-content: center }
->.org1 {background-color: rgba(20, 33, 38, 0.011)}`
-export const InformacoesLivro = styled.div`width: 98%; 
-
-> h1{font-size: 50px; color: green}
-> h3{}
-> p{}
-> button{margin-left: 50%; margin-top: 2%}`;
+>.org1 {background-color: rgba(20, 33, 38, 0.011); z-index: -1; position: relative}`
+export const InformacoesLivro = styled.div`width: 85%; 
+display: grid; padding: 5% 0px 0px 3%; text-align: justify;
+> h1{font-size: 40px; font-family: ${colors.titles}}
+> h3{font-family: ${colors.titles}}
+> p{font-family: ${colors.text}}
+> button{margin-left: 40%; margin-top: 2%; width: 200px; font-family: ${colors.text}}`;
 export const AreaDoLivreiro = styled.div`text-align: center; padding: 20px;
  background-color: #FCFCFC;
->h2{}
->button{}
+>h2{font-family: ${colors.titles}}
+>button{font-family: ${colors.text}}
 `
