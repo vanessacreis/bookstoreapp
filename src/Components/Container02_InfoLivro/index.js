437c6773b800ab3a350d383053297ff2f1b85544
Luzi <lu.gering@gmail.com>
2022-04-24 22:01:27 -0300

    Base para Func. do Livreiro./ Const p. ButtonsCRUD

--- src/Components/Container02_InfoLivro/index.js
+++ src/Components/Container02_InfoLivro/index.js
@@ -2,16 +2,44 @@ import styled from "styled-components";
 import { colors } from "../../Style/globalStyle.js";
 export const Pai = styled.div`display: grid;
 padding: 0px 0px 0px; bottom: 0px; 
->.org2 {display: grid; align-content: center; justify-content: center }
->.org1 {background-color: rgba(20, 33, 38, 0.011); z-index: -1; position: relative}`
-export const InformacoesLivro = styled.div`width: 85%; 
-display: grid; padding: 5% 0px 0px 3%; text-align: justify;
-> h1{font-size: 40px; font-family: ${colors.titles}}
-> h3{font-family: ${colors.titles}}
-> p{font-family: ${colors.text}}
-> button{margin-left: 40%; margin-top: 2%; width: 200px; font-family: ${colors.text}}`;
-export const AreaDoLivreiro = styled.div`text-align: center; padding: 20px;
- background-color: #FCFCFC;
->h2{font-family: ${colors.titles}}
->button{font-family: ${colors.text}}
+    >.org2 {
+        display: grid; align-content: center; 
+        justify-content: center }
+    >.org1 {
+        background-color: rgba(20, 33, 38, 0.011);}`
+export const InformacoesLivro = styled.div`
+width: 100%; display: grid; padding: 5% 0px 0px 3%; 
+text-align: justify; background-color: rgba(20, 33, 38, 0.02);
+    > h1{
+        font-size: 7vh; font-family: ${colors.titles};
+        color: ${colors.blue}
+    }
+    > h3{
+        font-family: ${colors.titles};
+        font-size: 3vh;
+    }
+    > p{
+        font-family: ${colors.text}; 
+        margin-right: 20%;
+        font-size: 3vh;}
+    > button{
+    margin-top: 2%; width: 24vh; height: 6vh;
+    font-family: ${colors.text}; background-color: #D9BFA0;
+    border: none; border-radius: 25px;
+    font-size: 2.7vh
+}`;
+export const AreaDoLivreiro = styled.div`
+text-align: center; padding: 0px;  height: 20vh;
+background-color: rgba(217, 191, 160, 0.07); display: block;
+>h2{font-family: ${colors.titles}; color: ${colors.orange}; 
+display: grid; align-content: start; margin-top: -0px;
+font-size:4vh}
+>.org2{
+    display: flex; flex-direction: row; justify-content: space-between; margin-top: 5%;
+}
+`;
+export const Buttons = styled.button`
+font-family: ${colors.text}; background-color:rgba(20, 33, 38, 0.08);
+border: none; border-radius: 25px;
+font-size: 2.7vh;  width: 24vh; height: 6vh; margin-top: 4%;
 `
