847f1e59bdbb80b76c93bc509a11f57915952f81
Luzi <lu.gering@gmail.com>
2022-04-24 21:59:07 -0300

    Alinhamento adeq. da imagem/ Nova Const dos El.

--- src/Components/Container01_InfoLivro/index.js
+++ src/Components/Container01_InfoLivro/index.js
@@ -1,21 +1,28 @@
 import styled from "styled-components";
 export const ColunaPai = styled.div `
 background-color: #D9BFA0;
-display: grid;
-padding: 0px;
-height: 87vh;
-position: -webkit-sticky;
-position: sticky;
-top: 0;
->ul{display: grid; padding: 0px}
+width: 100%;
+
 `
 export const Elementos = styled.div`
-    margin-left: 3%;
+    margin-left: 0px;
+    margin-top:5%;
+    margin-right: 5%;
+    position: -webkit-sticky;
+position: sticky;
+top: 0;
     >ul{
+        height: 86vh;
+    >li{
+        font-size: 4vh;
+       
+    }
     >img{
-        width:200px;
-        position: relative;
-        height: 300px;
+        width:87%;
+        height: 60vh;
+        object-fit: cover;
+        border-radius: 3%;
+        margin-left: 5px;
         }
     }
 `;
