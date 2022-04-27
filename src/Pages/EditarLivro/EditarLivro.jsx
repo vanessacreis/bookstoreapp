55ae146fbc9c492e65a96cf3fd6cd08b1b127d77
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-27 17:34:43 -0300

    Implementado as operações crud detele e update com put e update

--- src/Pages/EditarLivro/EditarLivro.jsx
+++ src/Pages/EditarLivro/EditarLivro.jsx
@@ -1,45 +1,16 @@
 import React from "react";
-// import Header from "../../Components/Header.jsx";
 import Footer from "../../Components/Footer/Footer.jsx";
-import FormInfoPubli from "../../Components/FormInfoPubli/FormInfoPubli.jsx";
 import * as S from "./EditarLivro.js";
-import { api } from "../../Services/Api.js";
-import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import FormEditar from "../../Components/FormEditar/FormEditar.jsx";
 
 export default function EditarLivro() {
   const { id } = useParams();
-  const [livro, setLivro] = useState([]);
-  const navigate = useNavigate();
-  // useEffect(() => {
-  //   api.get("books/book/" + id).then((response) => {
-  //     console.log(response);
-  //     setLivro(response.data.book[0]);
-  //   });
-  // }, [id]);
-
-  function handleOnChange(e) {
-    setLivro({ ...livro, [e.target.name]: e.target.value });
-    console.log(livro);
-  }
-  function handleUpdate() {
-    api.put("books/book/" + id, livro).then((response) => {
-      console.log(response);
-      setLivro(response.data);
-      navigate("/");
-    });
-  }
 
   return (
     <>
       <S.EditarLivro>
-        <FormInfoPubli
-          key={id}
-          id={id}
-          // onChange={handleOnChange}
-          // onClick={() => handleUpdate()}
-        />
-        ;
+        <FormEditar key={id} id={id} />;
       </S.EditarLivro>
 
       <Footer />
