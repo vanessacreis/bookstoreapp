e56c0ca614959ddbbaf222e2c8a14e9cf45f7a16
vanessacreis <vanessacreis@live.com>
2022-04-27 16:39:51 -0300

    Página da livreiro estilizada

--- src/Pages/Livreiro/Livreiro.jsx
+++ src/Pages/Livreiro/Livreiro.jsx
@@ -1,52 +1,17 @@
-import React, { useState, Component } from "react";
-import styles from "./Livreiro.module.css";
-import Navegar from "./Botao";
-
-function initialState() {
-  return { user: "", password: "" };
-}
+import React from "react";
+import * as S from "./livreiro.js";
+import FormLogin from "../../Components/FormLogin/FormLogin.jsx";
 
 const UserLogin = () => {
-  const [values, setValues] = useState(initialState);
-
-  function onChange(event) {
-    const { value, name } = event.target;
-
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  }
-
   return (
-    <div className={styles.userLogin}>
-      <h1 className={styles.userLoginTitle}>
-        LOGO DA <br></br>LIVRARIA
-      </h1>
-      <form autoComplete="nope">
-        <div className={styles.userLoginFormControl}>
-          <label className="nomeCargo">
-            Escolha seu cargo: <br />
-            <select>
-              <option value="estoquista">Estoquista</option>
-              <option value="gerente">Gerente</option>
-              <option value="livreiro">Livreiro</option>
-            </select>
-          </label>
-        </div>
-        <div className={styles.userLoginFormControl}>
-          <input
-            id="matricula"
-            type="text"
-            name="matricula"
-            onChange={onChange}
-            value={values.matricula}
-          />
-          <label htmlFor="matricula">Matrícula</label>
-        </div>
-      </form>
-      <Navegar />
-    </div>
+    <>
+      <S.Container>
+        <S.Login>
+          <img src="/assets/imgs/livraria-vermelho.svg" alt="" />
+          <FormLogin />
+        </S.Login>
+      </S.Container>
+    </>
   );
 };
 
---
+++ src/Pages/Livreiro/livreiro.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { colors } from "../../Style/globalStyle.js";
+
+export const Container = styled.main`
+  width: 100%;
+  height: calc(100vh - 125px);
+  background-image: url(/assets/imgs/BG-Livreiro.svg);
+  background-repeat: no-repeat;
+
+  @media (max-width: 700px) {
+    background-image: none;
+  }
+`;
+
+export const Login = styled.div`
+  width: 500px;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-right: 75px;
+  float: right;
+
+  > img {
+    width: 400px;
+    filter: drop-shadow(5px 5px 10px ${colors.white});
+  }
+`;
