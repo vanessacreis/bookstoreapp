ce800c55a36250bdf5aac23ae0bebbf6782a9b7f
vanessacreis <vanessacreis@live.com>
2022-04-28 00:03:06 -0300

    Componente botão ver mais

---
+++ src/Components/BotaoVerMais/BotaoVerMais.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../BotaoVerMais/BotaoVerMais.module.css";
+function BotaoVerMais() {
+  return (
+    <div className={styles.div}>
+      <button className={styles.button}>VER MAIS</button>
+    </div>
+  );
+}
+
+export default BotaoVerMais;
