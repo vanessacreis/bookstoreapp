7c8b29f63be1aafae0a25f74e7ab5287f4c56995
vanessacreis <vanessacreis@live.com>
2022-04-28 00:06:26 -0300

    Página de catálogo pronta e funcionando

---
+++ src/Pages/Catalogo/Catalogo.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import Input from "../../Components/InputCatalogo/Input.jsx";
+import { api } from "../../Services/Api.js";
+import ExibirLivros from "../../Components/ExibirLivros/ExibirLivros.jsx";
+import BotaoVerMais from "../../Components/BotaoVerMais/BotaoVerMais.jsx";
+import LargeFooter from "../../Components/LargeFooter/LargeFooter.jsx";
+import * as S from "./catalogo.js";
+import { Link } from "react-router-dom";
+
+function Catalogo() {
+  const [livro, setLivro] = useState([]);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    api.get("/books").then((response) => {
+      setLivro(response.data.books);
+      console.log(livro);
+    });
+  }, []);
+
+  return (
+    <>
+      <S.Container>
+        <S.Caixa>
+          <Input
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+          <S.Grid>
+            {livro
+              .filter((item) => {
+                if (input === "") {
+                  return item;
+                } else if (
+                  item.name.toLowerCase().includes(input.toLowerCase())
+                ) {
+                  return item;
+                }
+              })
+              .map((item) => {
+                return (
+                  <Link
+                    key={item.ID_books}
+                    to={`/info/${item.ID_books}`}
+                    className="link"
+                  >
+                    <ExibirLivros
+                      imagem={item.imagem}
+                      name={item.name}
+                      writer={item.writer}
+                    />
+                  </Link>
+                );
+              })}
+          </S.Grid>
+          <BotaoVerMais />
+        </S.Caixa>
+      </S.Container>
+      <LargeFooter />
+    </>
+  );
+}
+
+export default Catalogo;
---
+++ src/Pages/Catalogo/catalogo.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { colors } from "../../Style/globalStyle.js";
+
+export const Container = styled.main`
+  width: 100%;
+  height: auto;
+  min-height: 180vh;
+  background-image: url(/assets/imgs/BG-Catalogo.svg);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Caixa = styled.div`
+  width: 90%;
+  height: 90%;
+  margin-top: 3%;
+  margin-bottom: 3%;
+  border-radius: 10px;
+  background-color: ${colors.white}98;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Catalogo = styled.div`
+  width: 85%;
+`;
+
+export const Grid = styled.div`
+  width: 85%;
+  min-height: 150vh;
+  display: grid;
+  grid-template-columns: 25% 25% 25% 25%;
+  grid-template-rows: 33.33% 33.33% 33.33%;
+  grid-row-gap: 2%;
+  margin-top: 7%;
+
+  .link {
+    text-decoration: none;
+  }
+`;
