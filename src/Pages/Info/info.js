7e4a81c555d228b95e409f39fbdda882593d9070
vanessacreis <vanessacreis@live.com>
2022-04-28 00:10:17 -0300

    Remoção do background da página info

--- src/Pages/Info/info.js
+++ src/Pages/Info/info.js
@@ -1,14 +1,10 @@
 import styled from "styled-components";
-import { colors } from "../../Style/globalStyle";
+import { colors } from "../../Style/globalStyle.js";
 
 export const Main = styled.main`
   width: 100%;
   height: calc(100vh - 125px);
   display: flex;
   justify-content: space-between;
-  background-image: url(/assets/imgs/BG-Livros.svg);
-  background-repeat: no-repeat;
-  background-position: right;
-  background-size: contain;
   font-family: ${colors.text};
 `;
