700cd910ad3341e09e01c7d2d7ea9476965af463
vanessacreis <vanessacreis@live.com>
2022-04-28 21:49:32 -0300

    Botão de login

--- src/Components/FormLogin/formLogin.js
+++ src/Components/FormLogin/formLogin.js
@@ -30,3 +30,19 @@ export const Form = styled.form`
     padding: 5px;
   }
 `;
+
+export const Botao = styled.div`
+  margin-top: 25px;
+  width: 100px;
+  height: 30px;
+  background-color: ${colors.contrast};
+  color: ${colors.black};
+  font-family: ${colors.text};
+  font-size: 15px;
+  text-align: center;
+  border-style: none;
+  border-radius: 5px;
+  box-shadow: 2px 2px 5px;
+  align-self: center;
+  cursor: pointer;
+`;
